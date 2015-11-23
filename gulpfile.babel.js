c5daa29239d5ebb8a855c3623a375edf14607af9
Sam Gaus <sam@gaus.co.uk>
2015-11-23 11:45:26 +0000

    Moves built files to root directory for loopback interoperability.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,15 +5,15 @@ import path from 'path';
 
 const paths = {
   es6: ['src/*.js'],
-  es5: 'dist/',
-  sourceRoot: path.join(__dirname, 'es6'),
+  es5: '.',
+  sourceRoot: path.join(__dirname, 'dist'),
 };
 
 gulp.task('babel', () => {
   return gulp.src(paths.es6)
   .pipe(sourcemaps.init())
   .pipe(babel())
-  .pipe(sourcemaps.write('.', { sourceRoot: paths.sourceRoot }))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest(paths.es5));
 });
 
